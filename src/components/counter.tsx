10569af5767175821857a59da7d4ac193dea204c
sodai-watanabe <sodai@uniba.jp>
2024-01-13 16:17:24 +0900

    test

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -3,7 +3,6 @@ import { decrement, increment, reset } from '../features/counterSlice';
 import { RootState } from '../app/store';
 
 
-
 const Counter = () => {
     const count :number = useSelector((state:RootState) => state.counter.value);
     const dispatch = useDispatch();
